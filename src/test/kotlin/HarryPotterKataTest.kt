cf79ff01f4ceef9268202d22d1d282c09fc4c0a8
Squall Huang <selfi_star@yahoo.com.tw>
2021-06-12 01:37:32 +0800

    refactor buyBook function and priceShouldBe function

--- src/test/kotlin/HarryPotterKataTest.kt
+++ src/test/kotlin/HarryPotterKataTest.kt
@@ -1,13 +1,30 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class HarryPotterKataTest {
 
+    private lateinit var cart: Cart
+
+    @BeforeEach
+    fun setup() {
+        cart = Cart()
+    }
+
     @Test
     fun buy_1_normal_price() {
-        val cart = Cart()
-        val book = Book("1")
-        cart.add(book)
-        assertEquals(100,cart.totalPrice())
+        buyBook("1")
+        priceShouldBe(100)
+    }
+
+    private fun priceShouldBe(expectedPrice: Int) {
+        assertEquals(expectedPrice, cart.totalPrice())
+    }
+
+    private fun buyBook(vararg isbn: String) {
+        isbn.forEach {
+            val book = Book(it)
+            cart.add(book)
+        }
     }
 }
