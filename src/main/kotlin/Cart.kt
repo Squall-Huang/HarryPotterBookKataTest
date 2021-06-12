d4c67232a4decfafa941af6c95af32096d593d63
Squall Huang <selfi_star@yahoo.com.tw>
2021-06-12 16:47:53 +0800

    refactor discountLookup

--- src/main/kotlin/Cart.kt
+++ src/main/kotlin/Cart.kt
@@ -1,17 +1,17 @@
 class Cart {
 
-    var books = mutableListOf<Book>()
+    val books = mutableListOf<Book>()
+    val discountLookup = mapOf(1 to 1.0, 2 to 0.95)
 
     fun add(book: Book) {
         books.add(book)
     }
 
     fun totalPrice(): Int {
-        if (books.count() == 2) {
-            return (books.sumOf { 100.toInt() } * 0.95).toInt()
 
-        }
-        return books.sumOf { 100.toInt() }
+        val discount = discountLookup[books.count()] ?: 1.0
+        return (books.sumOf { 100.toInt() } * discount).toInt()
+
     }
 
 }
